98467f55ef8526d343065da7d6a896b16539fa53
Steven Seguin <sseguin@surveymonkey.com>
2017-01-18 09:59:42 -0500

    use consistent hash for etag

--- http_agent/utils/etag.py
+++ http_agent/utils/etag.py
@@ -1,3 +1,6 @@
+from zlib import adler32
+
+
 def make_entity_tag(body):
-    checksum = hash(body) + (1 << 64)
+    checksum = adler32(body.encode())
     return '"{checksum}"'.format(checksum=checksum)
